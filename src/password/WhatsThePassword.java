5a458064206f223ce205e8e33f85783568e55d29
Tilak Madichetti <tilak.madichetti@gmail.com>
2016-04-25 12:27:48 +0530

    Update WhatsThePassword.java

--- src/password/WhatsThePassword.java
+++ src/password/WhatsThePassword.java
@@ -1,8 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+_|          _|            _|      _|_|        _|_|_|                                          _|    _|                
+_|          _|    _|_|    _|    _|          _|          _|_|      _|_|_|  _|    _|  _|  _|_|      _|_|_|_|  _|    _|  
+_|    _|    _|  _|    _|  _|  _|_|_|_|        _|_|    _|_|_|_|  _|        _|    _|  _|_|      _|    _|      _|    _|  
+  _|  _|  _|    _|    _|  _|    _|                _|  _|        _|        _|    _|  _|        _|    _|      _|    _|  
+    _|  _|        _|_|    _|    _|          _|_|_|      _|_|_|    _|_|_|    _|_|_|  _|        _|      _|_|    _|_|_|  
+                                                                                                                  _|  
+                                                                                                              _|_|    
+- CEO and Founder - Tilak Maddy
+*/
+
 package password;
 
 import javafx.application.Application;
